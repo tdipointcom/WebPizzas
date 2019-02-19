c168f9881dec37c18c0b7d6db42e969f03d47975
Teddy DETEUF <teddy@tdipointcom.fr>
2019-02-19 15:09:52 +0100

    -

--- private/app/config/config.php
+++ private/app/config/config.php
@@ -3,7 +3,7 @@
  * Fichier de configuration général de l'application
  * 
  * 1. Définition des constantes
- * 2.
+ * 2. Définition des variables d'environnement d'éxécution
  * 3.
  */
 
@@ -13,3 +13,18 @@
 
 // WEBSITE_TITLE Définition du titre du site
 define("WEBSITE_TITLE","Web Pizzas !");
+
+
+/**
+ * 2. Définition des variables d'environnement d'éxécution
+ */
+
+// Environnement de développement ou de production ?
+// Les valeurs peuvent être "prod" ou "dev"
+// Par défaut, on considère que l'application s'exécute en environnement PROD
+$env = "prod";
+
+// Liste des domaines que l'on considère comme étant des environnements de développement
+$dev_domain = [
+    "127.0.0.1", "localhost", "webpizzas.local"
+];
