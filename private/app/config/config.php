16e68f7164496864bb7e167b419820fe111ff64d
Teddy DETEUF <teddy@tdipointcom.fr>
2019-02-19 14:56:28 +0100

    Third-2 upload

--- private/app/config/config.php
+++ private/app/config/config.php
@@ -1,1 +1,15 @@
-1
+<?php
+/**
+ * Fichier de configuration général de l'application
+ * 
+ * 1. Définition des constantes
+ * 2.
+ * 3.
+ */
+
+ /**
+  * 1. Définitions des constantes
+  */
+
+// WEBSITE_TITLE Définition du titre du site
+define("WEBSITE_TITLE","Web Pizzas !");
